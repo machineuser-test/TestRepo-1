3431d03c184cca3cd7ef35f3537043aa124239cc
arun-sunil <arun.sunil@amd.com>
2020-01-25 01:47:23 +0000

    Added new feature - multiply

--- calc.py
+++ calc.py
@@ -1,8 +1,14 @@
 def add(a, b):
     return a + b
 
+def mult(a, b)
+    return a * b
+
 num1 = 5
 num2 = 7
 
 sum = add(num1, num2)
 print("Sum = ", sum)
+
+prod = mult(num1, num2)
+print("Product = ", prod)
