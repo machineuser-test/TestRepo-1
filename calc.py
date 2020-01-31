08e82c763eedd8c725aa0a11b2269f3ca0e91926
Arun Sunil <41349380+as-icuro@users.noreply.github.com>
2020-01-30 16:16:18 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -4,6 +4,9 @@ def add(a, b):
 def mult(a, b):
     return a * b
 
+def div(a, b):
+    return a/b
+
 num1 = 5
 num2 = 7
 
@@ -12,3 +15,6 @@ print("Sum = ", sum)
 
 prod = mult(num1, num2)
 print("Product = ", prod)
+
+quot = div(num1, num2)
+print("Quotient = ", quot)
